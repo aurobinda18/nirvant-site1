9c53095937508ff398a75e0b0cb0722cc84592df
Aurobinda Nayak <nayakaurobinda618@gmail.com>
2025-11-01 18:45:43 +0000

    clg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    extend: {
+      colors: {
+        brand: {
+          yellow: colors.amber,
+          orange: colors.orange,
+          green: colors.emerald,
+          red: colors.rose,
+        },
+      },
+    },
+  },
   plugins: [],
 };
-
